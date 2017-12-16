81cfd286516181c0e406018048cce58c645995f9
hansika17 <hansika.kalra171@gmail.com>
2017-12-16 10:23:53 +0530

    check that enquiry is saved message sorry, got misplaced that
    Signed-off-by: hansika17 <hansika.kalra171@gmail.com>

--- index.php
+++ index.php
@@ -185,9 +185,10 @@ $conn->close();
 <label>Message:</label>
 <textarea placeholder="" style=" width:350px; overflow-y: scroll; height:200px;" type="text" name="enquiry_msg" ng-model="enquiry_msg" required></textarea>
 <br><span style="color:red" ng-show="myForm.enquiry_msg.$touched && myForm.enquiry_msg.$invalid">Message is required.</span>
-<br><span style="color:red">{{result}}</span>
 </div>
+<br>
 <button type="submit" class="btn" ng-click="formsubmit(myForm.$valid)"  ng-disabled="myForm.$invalid">Submit </button>
+<h4 style="color:red">{{result}}</h4>
 </form>
 
 </div></div>
