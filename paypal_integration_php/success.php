dfe6b7259d099edced0828d3004225d0b769cf72
RayTheoryHSM <hansika.kalra171@gmail.com>
2017-12-18 11:11:17 +0530

    success page UI

--- paypal_integration_php/success.php
+++ paypal_integration_php/success.php
@@ -1,33 +1,58 @@
-<?php
-include 'dbConfig.php';
 
-//Get payment information  PayPal
-$item_number = $_GET['itemfrom_number']; 
-$txn_id = $_GET['tx'];
-$payment_gross = $_GET['amt'];
-$currency_code = $_GET['cc'];
-$payment_status = $_GET['st'];
+	 <link rel="stylesheet" href="css/bootstrap.min.css">
+<style>
+	 #centerdiv {
+    position: fixed;
+    top: 20%;
+    left: 40%;
+    margin-top: -100px;
+    margin-left: -200px;
+}
+</style>
 
-//Get product price from database
-$productResult = $db->query("SELECT price FROM products WHERE id = ".$item_number);
-$productRow = $productResult->fetch_assoc();
-$productPrice = $productRow['price'];
+<div class='row'  id ='centerdiv'>
+<div class='row-md-12 row-xs-12'>	
+<img src='success.gif' align='center' alt=' payment success'></img>
+</div>
 
-if(!empty($txn_id) && $payment_gross == $productPrice){
-	//Check if payment data exists with the same TXN ID.
-    $prevPaymentResult = $db->query("SELECT payment_id FROM payments WHERE txn_id = '".$txn_id."'");
+<div class='row-md-12 row-xs-12'>	
+Hi Customer,
+</div>
+<?php echo $query;?>
+<div class='row-md-12 row-xs-12'>	
+We Have received your order with transaction number:<?php echo $_GET['txnid'];?>
+coursename:<?php echo $_GET['coursename'];?>
+Mode:<?php echo $_GET['mode'];?>
+</div>
 
-    if($prevPaymentResult->num_rows > 0){
-        $paymentRow = $prevPaymentResult->fetch_assoc();
-        $last_insert_id = $paymentRow['payment_id'];
-    }else{
-        //Insert tansaction data into the database
-        $insert = $db->query("INSERT INTO payments(item_number,txn_id,payment_gross,currency_code,payment_status) VALUES('".$item_number."','".$txn_id."','".$payment_gross."','".$currency_code."','".$payment_status."')");
-        $last_insert_id = $db->insert_id;
-    }
-?>
-	<h1>Your payment has been successful.</h1>
-    <h1>Your Payment ID - <?php echo $last_insert_id; ?>.</h1>
-<?php }else{ ?>
-	<h1>Your payment has failed.</h1>
-<?php } ?>
+<div class='row-md-12 row-xs-12'>	
+Thank you for the order.
+</div>
+
+</div>
+	
+	<?php
+require_once('database.php');	
+
+$msg = "Hi \n You have successfully applied to course from ratheory below are the details";
+
+foreach ($_GET as $key => $value) 
+	{
+		$enquiry_data[$key] = $value;
+		$msg .= "\n".$key.":".$value;
+	}
+$sql="select email,amount from rt_payment where txnid='".$_GET['txnid']."'";
+$execute=mysqli_query($conn,$sql);
+
+$result=mysqli_fetch_assoc($execute);
+$email=$result['email'];
+$headers = "From: contact@raytheory.com";
+$msg.="\n amount=".$result['amount'];
+
+mail($email,"RayTheory Course Payment Sucessfully",$msg,$headers);
+
+$query="update rt_payment set status='1' where txnid='".$_GET['txnid']."'"; 	
+mysqli_query($conn,$query);
+
+
+     ?>
