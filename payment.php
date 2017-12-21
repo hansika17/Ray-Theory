74a1cf02128997ebc4a3f0731074e06401a4cafc
RayTheoryHSM <hansika.kalra171@gmail.com>
2017-12-21 10:56:55 +0530

    Signed-off-by: RayTheoryHSM <hansika.kalra171@gmail.com>

--- payment.php
+++ payment.php
@@ -1,4 +1,87 @@
 <?php
+
+
+
+
+
+
+$myfile = fopen("store.prop", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
+
+
+
+$Pass = "PasswortPasswort";
+$Clear = "myownkey";        
+
+$crypted = fnEncrypt($Clear, $Pass);
+echo "Encrypred: ".$crypted."</br>";
+
+$newClear = fnDecrypt($crypted, $Pass);
+echo "Decrypred: ".$newClear."</br>";        
+
+function fnEncrypt($sValue, $sSecretKey)
+{
+    return rtrim(
+        base64_encode(
+            mcrypt_encrypt(
+                MCRYPT_RIJNDAEL_256,
+                $sSecretKey, $sValue, 
+                MCRYPT_MODE_ECB, 
+                mcrypt_create_iv(
+                    mcrypt_get_iv_size(
+                        MCRYPT_RIJNDAEL_256, 
+                        MCRYPT_MODE_ECB
+                    ), 
+                    MCRYPT_RAND)
+                )
+            ), "\0"
+        );
+}
+
+function fnDecrypt($sValue, $sSecretKey)
+{
+    return rtrim(
+        mcrypt_decrypt(
+            MCRYPT_RIJNDAEL_256, 
+            $sSecretKey, 
+            base64_decode($sValue), 
+            MCRYPT_MODE_ECB,
+            mcrypt_create_iv(
+                mcrypt_get_iv_size(
+                    MCRYPT_RIJNDAEL_256,
+                    MCRYPT_MODE_ECB
+                ), 
+                MCRYPT_RAND
+            )
+        ), "\0"
+    );
+}
+
+
+function fnDecryptForSingleValue($sValue)
+{
+    return rtrim(
+        mcrypt_decrypt(
+            MCRYPT_RIJNDAEL_256, 
+            $Pass, 
+            base64_decode($sValue), 
+            MCRYPT_MODE_ECB,
+            mcrypt_create_iv(
+                mcrypt_get_iv_size(
+                    MCRYPT_RIJNDAEL_256,
+                    MCRYPT_MODE_ECB
+                ), 
+                MCRYPT_RAND
+            )
+        ), "\0"
+    );
+}
+
+
+
 require_once('database.php');
 $service_provider = "payu_paisa";
 $MERCHANT_KEY = "gtKFFx";
